8f3494dcbe7609a89a0c8845873dc9c2bd40a584
Cesar Ciello <cesarciello15@gmai.com>
2020-03-26 22:13:01 -0300

    add: animation transitions

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,19 +1,55 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import React from 'react'
-import Logon from './pages/Logon';
-import Profile from './pages/Profile';
-import Register from './pages/Register';
-import NewIncident from './pages/NewIncident';
+import React from "react";
+import Logon from "./pages/Logon";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import NewIncident from "./pages/NewIncident";
+import { BrowserRouter, Route } from "react-router-dom";
+import { AnimatedSwitch, spring, AnimatedRoute } from "react-router-transition";
+
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`
+  };
+}
+
+function bounce(val) {
+  return spring(val, {
+    stiffness: 700,
+    damping: 70
+  });
+}
+
+const bounceTransition = {
+  atEnter: {
+    opacity: 0,
+    scale: 1.1
+  },
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.9)
+  },
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1)
+  }
+};
 
 export default function Routes() {
-    return(
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Logon} />
-                <Route path="/register" component={Register} />
-                <Route path="/profile" component={Profile} />
-                <Route path="/incidents/new" component={NewIncident} />
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <AnimatedSwitch
+        atEnter={bounceTransition.atEnter}
+        atLeave={bounceTransition.atLeave}
+        atActive={bounceTransition.atActive}
+        mapStyles={mapStyles}
+        className="route-wrapper"
+      >
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
+      </AnimatedSwitch>
+    </BrowserRouter>
+  );
 }
